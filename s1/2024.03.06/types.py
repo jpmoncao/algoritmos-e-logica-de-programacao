1a536e2c98776c97c73e785fbca799dc4897ae32
jpmoncao <pedrohjoao44@gmail.com>
2024-03-06 20:19:59 -0300

    feat: learn about boolean type

--- s1/2024.03.06/types.py
+++ s1/2024.03.06/types.py
@@ -1,5 +1,7 @@
 n1 = int(input('Digite um valor inteiro: '))
 n2 = float(input('Digite um valor real: '))
+bool = bool(input('Digite ou não um valor: '))
 
 print(f'Tipo n1 => {type(n1)}')
 print(f'Tipo n2 => {type(n2)}')
+print(f'O valor foi {bool} => {type(bool)}')
