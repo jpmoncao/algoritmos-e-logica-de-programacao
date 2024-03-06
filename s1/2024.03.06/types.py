aafcdd509718ff56611c516448ded6dbb510d8b7
jpmoncao <pedrohjoao44@gmail.com>
2024-03-06 20:18:20 -0300

    feat: learn about numeric types

--- s1/2024.03.06/types.py
+++ s1/2024.03.06/types.py
@@ -1,2 +1,5 @@
-n1 = int(input('Digite um valor: '))
-print(type(n1))
+n1 = int(input('Digite um valor inteiro: '))
+n2 = float(input('Digite um valor real: '))
+
+print(f'Tipo n1 => {type(n1)}')
+print(f'Tipo n2 => {type(n2)}')
