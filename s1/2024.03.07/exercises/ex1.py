d4c2a04131b4be52e96c90f942c0789031013731
jpmoncao <pedrohjoao44@gmail.com>
2024-03-07 22:17:01 -0300

    feat: finish exercise one about value of product

---
+++ s1/2024.03.07/exercises/ex1.py
@@ -0,0 +1,4 @@
+valor = float(input('Digite o valor do produto: '))
+qtde_parcelas = int(input('Digite a quantidade de parcelas: '))
+
+print(f'Serão {qtde_parcelas} parcelas de {valor / qtde_parcelas}')
