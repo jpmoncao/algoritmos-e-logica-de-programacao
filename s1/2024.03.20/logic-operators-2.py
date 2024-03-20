105a7ca40406d1c03212409260af6abc5d37113c
jpmoncao <pedrohjoao44@gmail.com>
2024-03-20 20:02:06 -0300

    feat: learn more about logic operators

---
+++ s1/2024.03.20/logic-operators-2.py
@@ -0,0 +1,13 @@
+print('Operador AND:')
+print(f'V e V -> {True and True}')
+print(f'V e F -> {True and False}')
+print(f'F e F -> {False and False}')
+
+print('\nOperador OR:')
+print(f'V ou V -> {True or True}')
+print(f'V ou F -> {True or False}')
+print(f'F ou F -> {False or False}')
+
+print('\nOperador NOT:')
+print(f'not V -> {not True}')
+print(f'not F -> {not False}')
