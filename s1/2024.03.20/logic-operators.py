8c66109083869ab2692e06f1a4d7022dc23c1865
jpmoncao <pedrohjoao44@gmail.com>
2024-03-20 19:53:41 -0300

    feat: learn about logic operators

---
+++ s1/2024.03.20/logic-operators.py
@@ -0,0 +1,20 @@
+n1 = int(input('Digite um inteiro: '))
+n2 = int(input('Digite outro inteiro: '))
+
+r = n1 > n2
+print(f'{n1} >  {n2} -> {r}')
+
+r = n1 < n2
+print(f'{n1} <  {n2} -> {r}')
+
+r = n1 == n2
+print(f'{n1} == {n2} -> {r}')
+
+r = n1 != n2
+print(f'{n1} != {n2} -> {r}')
+
+r = n1 >= n2
+print(f'{n1} >= {n2} -> {r}')
+
+r = n1 <= n2
+print(f'{n1} <= {n2} -> {r}')
